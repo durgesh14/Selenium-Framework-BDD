0aa63606e8fa8c3ca0e85012ce712b2be6597968
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-09-27 00:32:48 +0530

    Added code for browser setup

---
+++ src/main/java/com/framework/utility/DataReader.java
@@ -0,0 +1,27 @@
+package com.framework.utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DataReader {
+
+    public static final String filePath = System.getProperty("user.dir") + "/config.properties";
+
+    public static String getProperty(String key) throws IOException {
+        Properties prop = new Properties();
+        InputStream input = null;
+        String configValue = null;
+
+        try {
+            input = new FileInputStream(filePath);
+            prop.load(input);
+            configValue = prop.getProperty(key);
+        } finally {
+            input.close();
+        }
+        return configValue;
+    }
+}
--- src/main/java/com/framework/utility/TestCaseBase.java
+++ src/main/java/com/framework/utility/TestCaseBase.java
@@ -1,4 +1,35 @@
 package com.framework.utility;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class TestCaseBase {
+
+    @BeforeClass
+    public void browserSetup() throws IOException {
+        launchBrowser(DataReader.getProperty("url"));
+    }
+
+    public WebDriver launchBrowser(String url){
+        System.out.println("Opening Browser");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-infobars");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public void closeBrowser(WebDriver driver){
+        System.out.println("Quiting Browser");
+        driver.quit();
+    }
 }
